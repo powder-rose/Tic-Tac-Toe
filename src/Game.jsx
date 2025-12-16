2c5f2faa8dbcce5ed9c7005c8b20b48fad7cdd7f
Ekaterina Izotova <glittervodka@yandex.ru>
2025-12-16 08:11:31 +0300

    structure folders and components in folder components. rename App to Game

---
+++ src/Game.jsx
@@ -0,0 +1,11 @@
+import GameLayout from "../src/components/GameLayout.jsx";
+
+function Game() {
+  return (
+    <>
+      <GameLayout />
+    </>
+  );
+}
+
+export default Game;
---
+++ src/components/FieldLayout.jsx
@@ -0,0 +1,3 @@
+const FieldLayout = () => {};
+
+export default FieldLayout;
